d64454dcae61d259cd96d3d6346b31fe8d427307
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-06-06 21:20:41 -0700

    Fibonacci

--- main.m
+++ main.m
@@ -2,9 +2,19 @@
 
 :- interface.
 :- import_module io.
-
 :- pred main(io::di, io::uo) is det.
+
 :- implementation.
+:- import_module int.
+:- func fib(int) = int.
+
+fib(N) = (
+	if N =< 2
+	then 1
+	else fib(N - 1) + fib(N - 2)
+).
 
 main(!IO) :-
-	io.write_string("Hello, world!\n", !IO).
+	io.write_string("fib(17) = ", !IO),
+	io.write_int(fib(17), !IO),
+	io.nl(!IO).
