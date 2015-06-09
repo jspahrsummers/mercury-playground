c4861bab9388ce5e6dd13bc7a708cc785c769e80
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-06-09 14:06:06 -0700

    Write `cat` in Mercury

--- main.m
+++ main.m
@@ -6,62 +6,33 @@
 :- pred main(io::di, io::uo) is det.
 
 :- implementation.
-:- import_module char.
 :- import_module list.
 :- import_module string.
 
 main(!IO) :-
-	io.read_char(Result, !IO),
+	command_line_arguments(Files, !IO),
+	processFiles(Files, !IO).
+
+:- pred processFiles(list(string)::in, io::di, io::uo) is det.
+
+processFiles([], !IO) :- true.
+processFiles([X | Xs], !IO) :-
+	io.open_input(X, Resource, !IO),
 	(
-		Result = ok(Char),
-		io.write_char(rot13(Char), !IO),
-		main(!IO)
+		Resource = io.ok(Stream),
+		io.input_stream_foldl_io(Stream, io.write_char, Result, !IO),
+		(
+			if Result = io.error(ErrorCode)
+			then logError(ErrorCode, !IO)
+			else processFiles(Xs, !IO)
+		),
+		io.close_input(Stream, !IO)
 	;
-		Result = eof,
-		io.write_string("Bye!\n", !IO)
-	;
-		Result = error(ErrorCode),
-		io.format("%s\n", [s(io.error_message(ErrorCode))], !IO)
+		Resource = io.error(ErrorCode),
+		logError(ErrorCode, !IO)
 	).
 
-:- func rot13(char) = char.
+:- pred logError(error::in, io::di, io::uo) is det.
 
-rot13(CharIn) =
-	(
-		if is_upper(CharIn)
-		then rot13_upper(CharIn)
-		else rot13_lower(CharIn)
-	).
-
-:- func rot13_upper(char) = char.
-
-rot13_upper(CharIn) = to_upper(rot13_lower(to_lower(CharIn))).
-
-:- func rot13_lower(char) = char.
-
-rot13_lower(CharIn) =
-	(
-		if rot13_2(CharIn, ForwardChar)
-		then ForwardChar
-		else if rot13_2(BackwardChar, CharIn)
-		then BackwardChar
-		else CharIn
-	).
-
-:- pred rot13_2(char, char).
-:- mode rot13_2(in, out) is semidet.
-:- mode rot13_2(out, in) is semidet.
-
-rot13_2('a', 'n').
-rot13_2('b', 'o').
-rot13_2('c', 'p').
-rot13_2('d', 'q').
-rot13_2('e', 'r').
-rot13_2('f', 's').
-rot13_2('g', 't').
-rot13_2('h', 'u').
-rot13_2('i', 'v').
-rot13_2('j', 'w').
-rot13_2('k', 'x').
-rot13_2('l', 'y').
-rot13_2('m', 'z').
+logError(ErrorCode, !IO) :-
+	io.format("%s\n", [s(io.error_message(ErrorCode))], !IO).
