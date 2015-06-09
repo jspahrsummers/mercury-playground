86e1a8256c222b8457cc030639f514650ce92236
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-06-09 16:06:07 -0700

    Use a higher-order predicate for processing each file

--- main.m
+++ main.m
@@ -12,24 +12,28 @@
 
 main(!IO) :-
 	command_line_arguments(Files, !IO),
-	processFiles(Files, !IO).
+	with_each_file(echo_stream, Files, !IO).
 
-:- pred processFiles(list(string)::in, io::di, io::uo) is det.
+:- pred echo_stream(string::in, io.input_stream::in, io::di, io::uo) is det.
 
-processFiles([], !IO) :- true.
-processFiles([File | Files], !IO) :-
+echo_stream(File, Stream, !IO) :-
 	io.format("======== %s ========\n", [s(File)], !IO),
-
-	io.open_input(File, Resource, !IO),
-	guard_res(Resource, Stream),
-
 	io.input_stream_foldl_io(Stream, io.write_char, Result, !IO),
 	guard_res(Result),
+	io.nl(!IO).
 
+:- pred with_each_file(pred(string, io.input_stream, io, io), list(string), io, io).
+:- mode with_each_file(in(pred(in, in, di, uo) is det), in, di, uo) is det.
+
+with_each_file(_, [], !IO) :- true.
+with_each_file(Callback, [File | Files], !IO) :-
+	io.open_input(File, Result, !IO),
+	guard_res(Result, Stream),
+
+	Callback(File, Stream, !IO),
 	io.close_input(Stream, !IO),
-	io.nl(!IO),
 
-	processFiles(Files, !IO).
+	with_each_file(Callback, Files, !IO).
 
 :- pred guard_res(io.res(T)::in, T::out) is det.
 
